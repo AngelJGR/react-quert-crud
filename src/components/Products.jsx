1e58a322e9e046d77697bec2b69b9759c020cbdc
Angel Guerrero <ajguerreror@gmail.com>
2023-01-31 11:15:22 -0400

    useMutation delete

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,21 @@
 // import React from "react";
-import { useQuery } from 'react-query'
-import { getProducts } from '../api/productsAPI'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { deleteProduct, getProducts } from '../api/productsAPI'
 
 function Products () {
 
+    const queryClient = useQueryClient()
     const {isLoading, data: products, isError, error} = useQuery({
         queryKey: 'products',
         queryFn: getProducts,
         select: products => products.sort((a, b) => b.id - a.id)
+    })
+
+    const deleteProductMutation = useMutation({
+        mutationFn: deleteProduct,
+        onSuccess: () => {
+            queryClient.invalidateQueries('products')
+        }
     })
 
     if (isLoading) return <div>Loading...</div>
@@ -18,7 +26,7 @@ function Products () {
         <h3>{ product.name }</h3>
         <p>{ product.description }</p>
         <p>{ product.price }</p>
-        <button>delete</button>
+        <button onClick={() => deleteProductMutation.mutate(product.id)}>delete</button>
         <input type="checkbox" />
         <label htmlFor="">In Stock</label>
         <hr />
